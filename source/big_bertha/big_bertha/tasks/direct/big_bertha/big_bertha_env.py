09b47e2f88777c6092e0c163947b654b192ed14f
Yorha-7 <scientistn1420@gmail.com>
2026-06-03 12:24:29 +0530

    few minor changes

--- source/big_bertha/big_bertha/tasks/direct/big_bertha/big_bertha_env.py
+++ source/big_bertha/big_bertha/tasks/direct/big_bertha/big_bertha_env.py
@@ -96,8 +96,8 @@ class BigberthaEnv(DirectRLEnv):
             [
                 tensor
                 for tensor in (
-#                   self._robot.data.root_lin_vel_b,
-#                   self._robot.data.root_ang_vel_b,
+#                    self._robot.data.root_lin_vel_b,
+#                    self._robot.data.root_ang_vel_b,
                     self._robot.data.projected_gravity_b,
                     self._commands,
                     self._robot.data.joint_pos - self._robot.data.default_joint_pos,
